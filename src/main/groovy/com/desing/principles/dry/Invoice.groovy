388fd3b7d5594097e14f1fefacde8040d9536a4c
diegoazd <diegoazd@hotmail.com>
2016-11-22 20:59:12 -0600

    remove ;

--- src/main/groovy/com/desing/principles/dry/Invoice.groovy
+++ src/main/groovy/com/desing/principles/dry/Invoice.groovy
@@ -7,27 +7,26 @@ class Invoice {
   BigDecimal total
 
   String asciiStatement() {
-    StringBuffer result = new StringBuffer();
-    result.append("Bill for " + customer + "\n");
-    Iterator it = items.iterator();
+    StringBuffer result = new StringBuffer()
+    result.append("Bill for " + customer + "\n")
     items.each { lineItem ->
       result.append("\t" + lineItem.getProduct() + "\t\t"
-          + lineItem.getAmount() + "\n");
+          + lineItem.getAmount() + "\n")
     }
-    result.append("total owed:" + total + "\n");
-    return result.toString();
+    result.append("total owed:" + total + "\n")
+    return result.toString()
   }
 
   String htmlStatement() {
-    StringBuffer result = new StringBuffer();
-    result.append("<P>Bill for <I>" + customer + "</I></P>");
-    result.append("<table>");
+    StringBuffer result = new StringBuffer()
+    result.append("<P>Bill for <I>" + customer + "</I></P>")
+    result.append("<table>")
     items.each { lineItem ->
       result.append("<tr><td>" + lineItem.getProduct()
-          + "</td><td>" + lineItem.getAmount() + "</td></tr>");
+          + "</td><td>" + lineItem.getAmount() + "</td></tr>")
     }
-    result.append("</table>");
-    result.append("<P> total owed:<B>" + total + "</B></P>");
-    return result.toString();
+    result.append("</table>")
+    result.append("<P> total owed:<B>" + total + "</B></P>")
+    return result.toString()
   }
 }
