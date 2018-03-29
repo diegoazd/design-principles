ad48d7f8f286f4adf7ed57405ffb9ab54b1c7995
diegoazd <diegoazd@hotmail.com>
2018-03-29 08:52:47 -0600

    Add more case

---
+++ src/main/java/mx/com/kubo/bloaters/primitive/obsession/UserManagement.java
@@ -0,0 +1,49 @@
+package mx.com.kubo.bloaters.primitive.obsession;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class UserManagement {
+
+    public static final int ADMIN_ROLE = 1;
+    public static final int MANAGER_ROLE = 2;
+    public static final int CUSTOMER_SERVICE_ROLE = 3;
+    public static final int ACCOUNTANT_ROLE = 4;
+    public static final int USER_ROLE = 5;
+
+    private Map<String, User> users;
+
+    public UserManagement() {
+        users = new HashMap<>();
+        users.put("admin", new User("admin", "securePassword", ADMIN_ROLE));
+        users.put("manager", new User("manager", "securePassword", MANAGER_ROLE));
+        users.put("accountant", new User("accountant", "securePassword", ACCOUNTANT_ROLE));
+        users.put("user", new User("user", "securePassword", USER_ROLE));
+        users.put("customer_service", new User("customer_service", "securePassword", CUSTOMER_SERVICE_ROLE));
+    }
+
+
+    public User login(String user, String password) {
+        User loggedUser = users.get(user);
+
+        if(loggedUser != null && verifyUser(user, password, loggedUser)) {
+           return  loggedUser;
+        }
+
+        throw new RuntimeException("user not found");
+    }
+
+    private boolean verifyUser(String username, String password, User user) {
+       return user.getUser().equals(username) && user.getPassword().equals(password);
+    }
+
+    public User register(String user, String password) {
+        if(users.get(user) == null) {
+           return new User(user, password, USER_ROLE);
+        }else {
+            throw new RuntimeException("Users cant be registere");
+        }
+
+    }
+
+}
---
+++ src/test/java/mx/com/kubo/bloaters/primitive/obsession/UserManagementTest.java
@@ -0,0 +1,76 @@
+package mx.com.kubo.bloaters.primitive.obsession;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+public class UserManagementTest {
+
+    UserManagement userManagement;
+
+    @Before
+    public void setup() {
+        userManagement = new UserManagement();
+    }
+
+    @Test
+    public void shouldLoginAsAdministrator() {
+        User user = userManagement.login("admin", "securePassword");
+        assertNotNull(user);
+        assertEquals("admin", user.getUser());
+        assertEquals(UserManagement.ADMIN_ROLE, user.getUserRole());
+    }
+
+    @Test
+    public void shouldLoginAsManager() {
+        User user = userManagement.login("manager", "securePassword");
+        assertNotNull(user);
+        assertEquals("manager", user.getUser());
+        assertEquals(UserManagement.MANAGER_ROLE, user.getUserRole());
+    }
+
+    @Test
+    public void shouldLoginAsCustomerService() {
+        User user = userManagement.login("customer_service", "securePassword");
+        assertNotNull(user);
+        assertEquals("customer_service", user.getUser());
+        assertEquals(UserManagement.CUSTOMER_SERVICE_ROLE, user.getUserRole());
+    }
+
+    @Test
+    public void shouldLoginAsAccountant() {
+        User user = userManagement.login("accountant", "securePassword");
+        assertNotNull(user);
+        assertEquals("accountant", user.getUser());
+        assertEquals(UserManagement.ACCOUNTANT_ROLE, user.getUserRole());
+    }
+
+    @Test
+    public void shouldLoginAsUser() {
+        User user = userManagement.login("user", "securePassword");
+        assertNotNull(user);
+        assertEquals("user", user.getUser());
+        assertEquals(UserManagement.USER_ROLE, user.getUserRole());
+    }
+
+    @Test(expected = RuntimeException.class)
+    public void shouldThrowExceptionWHenUserNotFound() {
+        userManagement.login("foo", "securePassword");
+    }
+
+    @Test
+    public void shouldRegisterUser() {
+        User user = userManagement.register("abcde", "securePassword");
+        assertEquals("abcde", user.getUser());
+        assertEquals("securePassword", user.getPassword());
+        assertEquals(UserManagement.USER_ROLE, user.getUserRole());
+    }
+
+    @Test(expected = RuntimeException.class)
+    public void shouldThrowExceptionWhenRegisterUser() {
+        userManagement.register("admin", "securePassword");
+    }
+
+}
